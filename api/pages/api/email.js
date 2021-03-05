40632a566bbc2de73318705fafb6dacf9146ecf8
Gabriel-Aragao <gabriellamparina@gmail.com>
2021-03-05 01:52:16 -0300

    json file generation

--- api/pages/api/email.js
+++ api/pages/api/email.js
@@ -1,4 +1,17 @@
+import fs from 'fs';
 function postEmail(request, response){
+
+  const method = request.method;
+
+  switch(method){
+    case "POST":
+      const body = request.body;
+      fs.writeFileSync("./public/latestNews.json", JSON.stringify(body, null, 2), err => {
+        if(err){
+          console.log(err);
+        }
+      });
+  }
   response.json({"ok":true})
 }
 
