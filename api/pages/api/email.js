9beedb2bebdf88c251daac34d5850dad780e99f4
Gabriel-Aragao <gabriellamparina@gmail.com>
2021-03-05 02:45:47 -0300

    bug finded

--- api/pages/api/email.js
+++ api/pages/api/email.js
@@ -1,30 +1,14 @@
 import fs from 'fs';
+
 function postEmail(request, response){
 
-  try {
+
     const method = request.method;
     if(method === "POST"){
-      try {
-        const body = request.body;
-      } catch (error) {
-        response.json({"error": "can`t get body", "catched": error});
-      }
-      try {
-        fs.writeFileSync("./public/latestNews.json", JSON.stringify(body, null, 2), err => {
-          if(err){
-            response.json({"error": "can`t save file", "catched": err});
-          }
-        });
-      } catch (error) {
-        response.json({"error": "can`t use fs", "catched": error});
-      }
-      response.json({"created":true});
+      response.json(request.body);
     } else {
-        response.json({"ok":true})      
+      response.json({"ok":true});
     }
-  } catch (error) {
-    response.json({"error": "can`t switch", "method": method});
-  }
 
 }
 
