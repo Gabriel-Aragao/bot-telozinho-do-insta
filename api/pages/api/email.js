893a653d601112a0edf3d4c652ef18d02e1570e2
Gabriel-Aragao <gabriellamparina@gmail.com>
2021-03-06 02:23:41 -0300

    bug fixed

--- api/pages/api/email.js
+++ api/pages/api/email.js
@@ -1,17 +1,25 @@
 import fs from 'fs';
+
 async function postEmail(request, response){
 
   const method = request.method;
 
+
   switch(method){
     case "POST":
       const body = request.body;
-      
-      fs.writeFile("./public/latestNews.json", JSON.stringify(body, null, 2), err => {
-        if(err){
-          response.json({"error": "can`t save file", "catched": err});
-        }
-      });
+      const sender = body.envelope.from;
+      let content = body.html;
+      content = content.split("\r").join("");
+      content = content.split("\n").join("");
+      content = content.split("\t").join("");
+      content = content.split("\\").join("");
+      const res = {
+        "sender": sender,
+        "content": content
+      };
+
+      response.json(res);
   }
   response.json({"ok":true})
 }
